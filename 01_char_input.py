13ef399b99540eda404c8913c704d9d6e69cc6b7
The Fated One <wyrd0@WINDOWS-VLPIHKB.localdomain>
2018-05-11 16:52:49 -0500

    First challenge done!

--- 01_char_input.py
+++ 01_char_input.py
@@ -1,1 +1,7 @@
+from datetime import datetime
 
+name = 	input("Hello, what's your name?  ")
+age = 	int(input("How old are you, {}?  ".format(name)))
+hunna = int(datetime.now().year) + 100  - age
+
+print("You will turn 100 in the year {}, {}!".format(hunna, name))
