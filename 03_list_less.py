ce7a254bc7d0d54c741d5704130f1b11bf355190
J. Victor Elliott <wyrdspoon@gmail.com>
2018-05-13 14:15:43 -0500

    Third one done!

---
+++ 03_list_less.py
@@ -0,0 +1,17 @@
+import array
+
+while True:
+	c = input("Give a number:\n")
+	try:
+		c = int(c)
+	except ValueError:
+		print("{} is not a number".format(c))
+	if type(c) is int: break
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+n = []
+
+for e in a:
+	if e < c: n.append(e)
+
+print("{}".format(n))
